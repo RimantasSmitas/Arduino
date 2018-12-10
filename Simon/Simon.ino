d4e7ea589f187d26c021de93bcdc11adf591b3d5
rimantas <rimantas.smitas@gmail.com>
2018-12-10 12:24:43 +0100

    Update Simon

--- Simon/Simon.ino
+++ Simon/Simon.ino
@@ -1,15 +1,3 @@
-/*
-//#define YELLOW 0
-//#define GREEN 1
-//#define RED 2
-//#define BLUE 3
-//int nrOfLightsOn = 0;
-//#define BUTTON1 13
-//#define BUTTON2 12
-//#define BUTTON3 11
-//#define BUTTON4 10
-*/
-
 #define SPEAKER 9
 int level;
 
@@ -22,8 +10,11 @@ struct gamePadType {
 //Notes and their frequencies 
 //261, 277, 294, 311, 330, 349, 370, 392, 415, 440};
 //mid C  C#   D    D#   E    F    F#   G    G#   A
-//gamePadType Color = {PinNrLed, Frequency, PinNrButton};
 */
+
+//If one wants to add more colours he/she should add a line here with the following format 
+//gamePadType Color = {PinNrLed, Frequency, PinNrButton};
+//Once that is done you should add  the new pad to the array and incriment the number of colors counter
 gamePadType Yellow = {0, 440, 10};
 gamePadType Green = {1, 392, 11};
 gamePadType Red = {2, 349, 12};
@@ -35,6 +26,7 @@ int numberOfColours=4;//Number of items in array
 int memoryArray[250];
 
 void setup() {
+  Serial.begin(9600);
   for(int i=0;i<numberOfColours;i++){//loops through the array of GamePad
   pinMode(GamePad[i].PinNrLed, OUTPUT);//Led Pin To OutPut 
   pinMode(GamePad[i].PinNrButton, INPUT_PULLUP); // button is connected to ground
@@ -42,23 +34,26 @@ void setup() {
   level = 0;
   Testfunction();
 }
-
+//plays a specific sound 
 void PlaySoundOn(gamePadType i) {
   tone(SPEAKER, i.Frequency);
 }
 
-void PlaySoundOff(gamePadType i) {
+//silences the speaker
+void PlaySoundOff() {
   noTone(SPEAKER);
 }
 
+//turns on the specific pad led
 void TurnLighOn(gamePadType i) {
   digitalWrite(i.PinNrLed, HIGH);
 }
-
+//turns off the specific pad led
 void TurnLighOff(gamePadType i) {
   digitalWrite(i.PinNrLed, LOW);
 }
 
+//blinks and buzzez specific pad 
 void BlinkAndBuzz(gamePadType i) {
   delay(500);
   tone(SPEAKER, i.Frequency);
@@ -68,63 +63,87 @@ void BlinkAndBuzz(gamePadType i) {
   digitalWrite(i.PinNrLed, LOW);
 }
 
+//turns on all leds 
 void AllOn() {
   for(int i=0;i<numberOfColours;i++){//loops through the array of GamePad
-  digitalWrite(GamePad[i].PinNrLed, HIGH);//Led Pin To OutPut 
+    digitalWrite(GamePad[i].PinNrLed, HIGH);//Led Pin To OutPut 
   }
 }
+
+//turns off all leds 
 void AllOff() {
   for(int i=0;i<numberOfColours;i++){//loops through the array of GamePad
-  digitalWrite(GamePad[i].PinNrLed, LOW);//Led Pin To OutPut 
+     digitalWrite(GamePad[i].PinNrLed, LOW);//Led Pin To OutPut 
   }
   noTone(SPEAKER);
 }
+
+// plays  all the pads that are placed in to memory
 void PlayMemory(){
   for(int i=0;i<level+1;i++){
-  BlinkAndBuzz(GamePad[i]);
+    BlinkAndBuzz(GamePad[memoryArray[i]]);
+    Serial.println(memoryArray[i]);
   }
 }
-//to be developed
-void WaitForInput(){
-  for(int i=0;i<level+1;i++){  
-  }
-  }
 
-void loop() {
-   testSoundWithLight();
-  //testLightWithSoundSimultanious();
-  /* int newrandom = random(0,numberOfColours);
+
+//This function loops through the array of levels and the set pads
+//Then waits for input for each level 
+//if the input is incorrect it starts over the program. 
+
+void WaitForInput(){
+    int input;
+    for(int x=0;x<level+1;){ 
+      input=numberOfColours; 
+      int comp = memoryArray[x];
+      while(input == numberOfColours){
+        for(int i=0; i<numberOfColours; i++){
+          if(digitalRead(GamePad[i].PinNrButton)==0){
+            while(digitalRead(GamePad[i].PinNrButton)==0){
+              PlaySoundOn(GamePad[i]);
+              TurnLighOn(GamePad[i]);
+              input = i;}
+            PlaySoundOff();
+            TurnLighOff(GamePad[i]);            
+            if(comp==input)
+              {
+              x++;             
+              }
+            else{
+               YouLost();
+               level = 0;
+               return;              
+              }  
+          }
+      }
+     }
+   }
+    level++; 
+    return;
+}
+
+void loop() { 
+ // testSoundWithLight();
+ // testLightSimultanious();
+   int newrandom = random(0,numberOfColours);
    memoryArray[level] = newrandom;
    PlayMemory(); 
    WaitForInput(); 
-  */
- /*   if (0 == digitalRead(BUTTON1))
-    {
-      digitalWrite(BLUE,LOW);
-      nrOfLightsOn--;
-    }else{}
-     if (0 == digitalRead(BUTTON2))
-    {
-      digitalWrite(RED,LOW);
-      nrOfLightsOn--;
-    }else{}
-    if (0 == digitalRead(BUTTON3))zzZzz                                                                                              
-    {
-      digitalWrite(GREEN,LOW);
-      nrOfLightsOn--;
-    }else{}
-    if (0 == digitalRead(BUTTON4))
-    {
-     digitalWrite(YELLOW,LOW);
-     nrOfLightsOn--;
-    }
-    if (nrOfLightsOn<1)
-    {
-    delay(500);
-    nrOfLightsOn=4;
-    digitalWrite(YELLOW,HIGH);
-    digitalWrite(GREEN,HIGH);
-    digitalWrite(RED,HIGH);
-    digitalWrite(BLUE,HIGH);
-  */
 }
+
+//You lost funtion blinks all leds with a specific tone then clears the memory array.
+void YouLost(){
+  for (int i = 0; i<3;i++){
+    delay(500);
+    tone(SPEAKER, 261);
+    AllOn();
+    delay(500);
+    noTone(SPEAKER);
+    AllOff();
+  }
+  for(int i = 0; i<level;i++){
+    memoryArray[i]=5;
+  }
+  }
+
+
--- Simon/Test.ino
+++ Simon/Test.ino
@@ -1,64 +1,56 @@
+// test function to test all features 
 void Testfunction() {
-  delay(500);
-  BlinkAll();
-  BlinkAllSeries();
-  BlinkAndBuzzAllSeries();
-  
- // testSoundWithLight();
-  delay(500);
+   delay(500);
+   BlinkAll();
+   BlinkAllSeries();
+   BlinkAndBuzzAllSeries();
+ //testSoundWithLight();
+   delay(500);
 }
-
+//blinks a specific pad
 void blink(gamePadType i) {
-  digitalWrite(i.PinNrLed, HIGH);
-  delay(500);
-  digitalWrite(i.PinNrLed, LOW);
-  delay(500);
+   digitalWrite(i.PinNrLed, HIGH);
+   delay(500);
+   digitalWrite(i.PinNrLed, LOW);
+   delay(500);
 }
-
+// blinks all leds
 void BlinkAll() {
-  delay(500);
-  AllOn();
-  delay(500);
-  AllOff();
-  delay(500);
-  AllOn();
-  delay(500);
-  AllOff();
-  delay(500);
+   delay(500);
+   AllOn();
+   delay(500);
+   AllOff();
+   delay(500);
+   AllOn();
+   delay(500);
+   AllOff();
+   delay(500);
 }
-
+//blinks all pads one by one 
 void BlinkAllSeries() {
-  for(int i=0;i<numberOfColours;i++)
-  blink(GamePad[i]);
-  delay(500);
-  /*
-  blink(Yellow);
-  delay(500);
-  blink(Green);
-  delay(500);
-  blink(Red);
-  delay(500);
-  blink(Blue);
-*/
-}
-
+   for(int i=0;i<numberOfColours;i++)
+   blink(GamePad[i]);
+   delay(500);
+ }
+//blinks and buzzes all pads one by one 
 void BlinkAndBuzzAllSeries() {
-  for(int i=0;i<numberOfColours;i++)
-  BlinkAndBuzz(GamePad[i]);
-  delay(500);
+   for(int i=0;i<numberOfColours;i++)
+   BlinkAndBuzz(GamePad[i]);
+   delay(500);
 }
 
 //  Plays the whole frequency range
 void TestSpeaker(){
-      for (int i = 0; i < 5500; i++)
+    for (int i = 0; i < 5500; i++)
     {
       tone(SPEAKER, i);
       delay(20);
     }
-  noTone(SPEAKER);
+    noTone(SPEAKER);
 }
 
 //When the button is pushed a coresponding light with tune is played.
+//Only works one at a time 
 void testSoundWithLight()
 {
    while (0 == digitalRead(Yellow.PinNrButton))
@@ -83,10 +75,13 @@ void testSoundWithLight()
     TurnLighOn(Blue);
   }
   AllOff();
-
-
 }
-void testLightWithSoundSimultanious(){
+
+
+
+//When this function is active it checks for inputs and lights up the coresponding led
+// buttons with lights can be tested simultaniously 
+void testLightSimultanious(){
     int y,g,r,b;
   if (1 == digitalRead(Yellow.PinNrButton))
   {
@@ -131,65 +126,53 @@ void testLightWithSoundSimultanious(){
 
   if (0 == digitalRead(Green.PinNrButton))
   {
-    PlaySoundOn(Green);
     TurnLighOn(Green);
   }
   else{
-    PlaySoundOff(Green);
     TurnLighOff(Green);
   }
 
   if (0 == digitalRead(Red.PinNrButton))
   {
-    PlaySoundOn(Red);
     TurnLighOn(Red);
   }
   
   else{
-    PlaySoundOff(Red);
     TurnLighOff(Red);
   }
   if (0 == digitalRead(Blue.PinNrButton))
   {
-    PlaySoundOn(Blue);
     TurnLighOn(Blue);
   }
   
   else{
-    PlaySoundOff(Blue);
     TurnLighOff(Blue);
   }
 
-if(y==1){
-  PlaySoundOn(Yellow);
-  TurnLighOn(Yellow);
+  if(y==1){
+    TurnLighOn(Yellow);
   }
- if(y==0){
-    PlaySoundOff(Yellow);
+  if(y==0){
     TurnLighOff(Yellow);
   }
-if(g==1){
-  PlaySoundOn(Green);
-  TurnLighOn(Green);
+  if(g==1){
+    TurnLighOn(Green);
   }
- if(g==0){
-    PlaySoundOff(Green);
+  if(g==0){
     TurnLighOff(Green);
   }
-if(r==1){
-  PlaySoundOn(Red);
-  TurnLighOn(Red);
+  if(r==1){
+    TurnLighOn(Red);
   }
- if(r==0){
-    PlaySoundOff(Red);
+  if(r==0){
     TurnLighOff(Red);
   }
   if(b==1){
-  PlaySoundOn(Blue);
-  TurnLighOn(Blue);
+    TurnLighOn(Blue);
   }
  if(b==0){
-    PlaySoundOff(Blue);
     TurnLighOff(Blue);
   }
 }
+
+
